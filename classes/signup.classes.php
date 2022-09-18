c10e2a2dba74371f5487f628ce586ff27642643f
Gabriel Biacchi <gabrielbiacchi.gb@gmail.com>
2022-09-18 00:10:42 -0300

    Last thing we did was creating the signup.classes.php file

--- classes/signup.classes.php
+++ classes/signup.classes.php
@@ -2,5 +2,11 @@
 
 // Here we are going to run queries
 class Signup extends dbh {
+    protected function checkUser($uid, $email) {
+        // stmt stands for "statement"
+        // We use the prepare method and the query with the interrogation signs to prevent us from injection attacks
+        $stmt  = $this->connect()->prepare('SELECT users_uid FROM users WHERE users_uid = ? OR users_email = ?;');
 
+        if($stmt->execute)
+    }
 }
